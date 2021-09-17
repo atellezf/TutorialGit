561c47c12028c2ee5c9bda48ddfe1fe93d55a8c6
alejandro <alejandro.tellez@gmail.com>
2021-09-17 16:57:17 -0500

    Se reemplaza StringBuilder por String Blocks

--- src/edu/utel/tutorial/Persona.java
+++ src/edu/utel/tutorial/Persona.java
@@ -3,10 +3,11 @@ package edu.utel.tutorial;
 public record Persona(String nombre, String apellidos) {
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("Persona{");
-        sb.append("nombre='").append(nombre).append('\'');
-        sb.append(", apellidos='").append(apellidos).append('\'');
-        sb.append('}');
-        return sb.toString();
+        return """
+                Persona {
+                    nombre='%s',
+                    apellidos='%s'
+                }
+                """.formatted(nombre, apellidos);
     }
 }
