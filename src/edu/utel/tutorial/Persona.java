c5dadc79b6c07757152da8c062966e770751196a
alejandro <alejandro.tellez@gmail.com>
2021-09-17 08:49:27 -0500

    Proyecto Inicial

---
+++ src/edu/utel/tutorial/Persona.java
@@ -0,0 +1,37 @@
+package edu.utel.tutorial;
+
+public class Persona {
+
+    private String nombre;
+    private String apellidos;
+
+    public Persona(String nombre, String apellidos) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Persona{");
+        sb.append("nombre='").append(nombre).append('\'');
+        sb.append(", apellidos='").append(apellidos).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ src/edu/utel/tutorial/Programa.java
@@ -0,0 +1,8 @@
+package edu.utel.tutorial;
+
+public class Programa {
+    public static void main(String[] args) {
+        Persona pa = new Persona("Benito", "Bodoque");
+        System.out.println(pa);
+    }
+}
